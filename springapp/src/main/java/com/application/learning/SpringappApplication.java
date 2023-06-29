fe6cc6973e546f40e726c95d82ac731daeca8d13
mrinal05aeccse <mrinal05aeccse@gmail.com>
2023-06-29 05:45:24 +0000

    Renamed The Main file

--- springapp/src/main/java/com/application/learning/SpringappApplication.java
+++ springapp/src/main/java/com/application/learning/SpringappApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"main.java.com.application.learning.Models"})
-public class SpringappApringApplication {
+public class SpringappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
