52352c884756f8616abceba7169da9830224798d
mrinal05aeccse <mrinal05aeccse@gmail.com>
2023-06-29 07:40:53 +0000

    Done

--- springapp/src/main/java/com/application/learning/Services/Payment/PaymentService.java
+++ springapp/src/main/java/com/application/learning/Services/Payment/PaymentService.java
@@ -1,6 +1,7 @@
 package main.java.com.application.learning.Services.Payment;
 import main.java.com.application.learning.Exception.ResourceNotFoundException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
 import main.java.com.application.learning.Repository.Payment.PaymentRepository;
@@ -9,6 +10,11 @@ import main.java.com.application.learning.Models.Payment.PaymentModel;
 @Service
 public class PaymentService {
     private PaymentRepository paymentRepository;
+
+    @Autowired
+    public PaymentService(PaymentRepository paymentRepository){
+        this.paymentRepository=paymentRepository;
+    }
 
     public PaymentModel savePayment(PaymentModel paymentModel){
         return paymentRepository.save(paymentModel);
