657ac97452dd9d014bc31c5780e9dba10167c21b
GSanthoshG <chitrabangarusamy1030@gmail.com>
2023-07-03 14:35:13 +0000

    commited

--- springapp/src/main/java/com/example/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/example/springapp/SpringappApplication.java
@@ -2,10 +2,6 @@ package com.example.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
-=======
-import org.springframework.boot.autoconfigure.domain.EntityScan;
->>>>>>> a415e4511cc1ee8e5a57625447e5b66efc054e84
 
 @SpringBootApplication
 public class SpringappApplication {
@@ -13,8 +9,4 @@ public class SpringappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
 	}
-<<<<<<< HEAD
-=======
-
->>>>>>> a415e4511cc1ee8e5a57625447e5b66efc054e84
 }
