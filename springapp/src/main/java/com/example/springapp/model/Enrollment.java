00784e5fcf7d091208666070ca6c28b64b7d96d4
GSanthoshG <chitrabangarusamy1030@gmail.com>
2023-07-04 11:47:14 +0000

    commited

---
+++ springapp/src/main/java/com/example/springapp/model/Enrollment.java
@@ -0,0 +1,12 @@
+package main.java.com.example.springapp.model;
+
+public class Enrollment {
+    private Long id;
+    private Long courseid;
+    private Long Instructorid;
+    
+    public Enrollment() {
+    }
+    
+    
+}
