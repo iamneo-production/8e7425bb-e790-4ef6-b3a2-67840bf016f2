fe42b06c2a516a855f5f2ae8cbe243885c89471c
Joshika11 <Joshikakalamadugu@gmail.com>
2023-07-07 12:54:20 +0000

    updated

--- springapp/src/main/java/com/example/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/example/springapp/SpringappApplication.java
@@ -2,19 +2,11 @@ package com.example.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
-
-=======
 import org.springframework.boot.autoconfigure.domain.EntityScan;
->>>>>>> c737eb22183f83eeb3461c1570f602b766a530a4
 @SpringBootApplication
 public class SpringappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
 	}
-<<<<<<< HEAD
-
-=======
->>>>>>> c737eb22183f83eeb3461c1570f602b766a530a4
 }
