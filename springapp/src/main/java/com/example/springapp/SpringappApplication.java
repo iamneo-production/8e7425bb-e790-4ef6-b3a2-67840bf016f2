6f79c4b74c59c67e450569ec15a7ef143cd26162
mrinal05aeccse <mrinal05aeccse@gmail.com>
2023-07-07 09:38:41 +0000

    done

--- springapp/src/main/java/com/example/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/example/springapp/SpringappApplication.java
@@ -1,9 +1,11 @@
 package com.example.springapp;
 
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+
 public class SpringappApplication {
 
 	public static void main(String[] args) {
