ec423cf0a4f773ba8c76fcea9dcde20bbe7d9f41
Sumanth42 <nsumanth2112@gmail.com>
2023-07-25 19:31:05 +0000

    added instructor dashboard

--- springapp/src/main/java/com/example/springapp/repository/EnrollmentRepository.java
+++ springapp/src/main/java/com/example/springapp/repository/EnrollmentRepository.java
@@ -1,9 +1,27 @@
 package com.example.springapp.repository;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 
 import com.example.springapp.model.Enrollment;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment,Long>{
 	
+     @Query(value = "select * from enrollments where user_id=?1",nativeQuery = true)
+	List<Enrollment> findEnrollmentsByUserId(Long userId);
+     
+     @Query(value = "select * from enrollments where course_id=?1",nativeQuery = true)
+	List<Enrollment> findEnrollmentsByCourseId(Long courseId);
+     
+
+     @Transactional
+     @Modifying
+     @Query(value="DELETE FROM enrollments e WHERE e.course_id = :courseId",nativeQuery = true)
+     void deleteAllByCourseId(Long courseId);
 }
