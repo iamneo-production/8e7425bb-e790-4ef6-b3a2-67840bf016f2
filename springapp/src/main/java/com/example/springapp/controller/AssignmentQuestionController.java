056513ab8a7d2a9178c514dde6a5f80b536e3665
RithickSaran <rithickkrishna397@gmail.com>
2023-07-25 21:32:01 +0000

    added Assignment API

---
+++ springapp/src/main/java/com/example/springapp/controller/AssignmentQuestionController.java
@@ -0,0 +1,37 @@
+package com.example.springapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springapp.model.AssignmentQuestion;
+import com.example.springapp.service.AssignmentQuestionService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/assignmentQuestion")
+public class AssignmentQuestionController {
+    @Autowired
+    public AssignmentQuestionService assignmentQuestionService;
+
+    @GetMapping("/getbyAssignmentId/{assignmentId}")
+    public ResponseEntity<List<AssignmentQuestion>> getByAssignmentId(@PathVariable("assignmentId") Long assignmentId){
+        List<AssignmentQuestion> assignmentQuestions = assignmentQuestionService.getAllAssignmentQuestionsByAssignmentId(assignmentId);
+        return new ResponseEntity<List<AssignmentQuestion>>(assignmentQuestions, HttpStatus.OK);
+    }
+
+    @PostMapping("/addQuestion")
+    public ResponseEntity<AssignmentQuestion> addQuestion(@RequestBody AssignmentQuestion assignmentQuestion){
+        AssignmentQuestion addedQuestion = assignmentQuestionService.addQuestion(assignmentQuestion);
+        return new ResponseEntity<AssignmentQuestion>(addedQuestion, HttpStatus.OK);
+    }
+}
