60beac3a741a36828ca343b9389410f225d98120
Rameshkumar30 <vrameshkumar444@gmail.com>
2023-07-25 21:00:56 +0000

    Courses and Lessons API updated

--- springapp/src/main/java/com/example/springapp/repository/CourseRepository.java
+++ springapp/src/main/java/com/example/springapp/repository/CourseRepository.java
@@ -1,6 +1,9 @@
 package com.example.springapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.springapp.model.Course;
