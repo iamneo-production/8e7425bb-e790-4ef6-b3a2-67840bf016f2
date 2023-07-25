e71072a3ba3032e40b84b371f27f44b9251106ea
mrinal05aeccse <mrinal05aeccse@gmail.com>
2023-07-25 17:51:56 +0000

    paymnet part done

--- angularapp/src/app/components/Payment_components/payment-report/payment-report.component.ts
+++ angularapp/src/app/components/Payment_components/payment-report/payment-report.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Payment } from 'src/app/models/payment';
+import { PaymentService } from 'src/app/services/payment.service';
 
 @Component({
   selector: 'app-payment-report',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./payment-report.component.css']
 })
 export class PaymentReportComponent {
+  payments: Payment[] = [];
+  user_id: number = Number(localStorage.getItem('id'));
 
+  constructor(private paymentService: PaymentService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.getPayments(this.user_id);
+  }
+
+  //to get payment details
+  private getPayments(id: number) {
+    this.paymentService.getPaymentList(id).subscribe(data => {
+      this.payments = data;
+    })
+  }
+
+  //to redirect to home page on back button click
+  onback() {
+    this.router.navigate(['/studentHome'])
+  }
 }
--- angularapp/src/app/models/payment.ts
+++ angularapp/src/app/models/payment.ts
@@ -1,2 +1,7 @@
-export class Payment {
+export interface Payment{
+    id:number;
+    userId:number;
+    amount:number;
+    courseId:number;
+    date: Date;
 }
