760ea849045b79a930cba663c589cc25c1796c48
Rameshkumar30 <vrameshkumar444@gmail.com>
2023-07-25 17:22:41 +0000

    Updated

--- angularapp/src/app/services/course.service.ts
+++ angularapp/src/app/services/course.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Course } from '../models/course';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CourseService {
+export class CourseServiceImpl {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+
+  //to get all courses
+  public getCourses(): Observable<Course[]> {
+    let url = `${this.baseUrl}/course`
+    return this.http.get<Course[]>(url);
+  }
+
+  //to post new course
+  public addCourses(course: Course): Observable<Course> {
+    let url = `${this.baseUrl}/course`
+    return this.http.post<Course>(url, course);
+  }
+
+  //to update course
+  public updateCourses(course: Course): Observable<Course> {
+    let url = `${this.baseUrl}/course/update`
+    return this.http.put<Course>(url, course);
+  }
+
+  //to delete course
+  public deleteCourses(courseId: number): Observable<any> {
+    let url = `${this.baseUrl}/course/delete/${courseId}`
+    return this.http.delete<any>(url);
+  }
+
+  //to get courses by instrucctor id
+  public getCoursebyInstructorId(): Observable<Course[]> {
+    let instructorId = localStorage.getItem('id');
+    let url = `${this.baseUrl}/course/instructor/${instructorId}`;
+    return this.http.get<Course[]>(url);
+  }
+
+  //to get specific course by id
+  public getCourseByCourseId(courseId: number): Observable<any> {
+    let url = `${this.baseUrl}/course/${courseId}`;
+    return this.http.get<any>(url);
+  }
+
 }
