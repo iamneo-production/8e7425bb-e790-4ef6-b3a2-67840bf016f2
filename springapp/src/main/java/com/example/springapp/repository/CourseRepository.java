7bf1bd7a70078419e026bd6169fe4e0de98b1301
GSanthoshG <chitrabangarusamy1030@gmail.com>
2023-07-27 06:23:12 +0000

    commited the resolved enrollment deletion part

--- springapp/src/main/java/com/example/springapp/repository/CourseRepository.java
+++ springapp/src/main/java/com/example/springapp/repository/CourseRepository.java
@@ -1,13 +1,14 @@
 package com.example.springapp.repository;
 
 import java.util.List;
-
+import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.springapp.model.Course;
 
+@Transactional
 @Repository
 public interface CourseRepository extends JpaRepository<Course,Long> {
     
