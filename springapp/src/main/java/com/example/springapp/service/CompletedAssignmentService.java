c35330fcc2852e799b7114a542ca5f1fdbc2ea0d
Rishik04 <rishikcharan04@gmail.com>
2023-07-30 11:28:50 +0000

    fixed

--- springapp/src/main/java/com/example/springapp/service/CompletedAssignmentService.java
+++ springapp/src/main/java/com/example/springapp/service/CompletedAssignmentService.java
@@ -8,29 +8,28 @@ import org.springframework.stereotype.Service;
 import com.example.springapp.model.CompletedAssignments;
 import com.example.springapp.repository.CompletedAssignmentRepository;
 
-
 @Service
 public class CompletedAssignmentService {
     @Autowired
     public CompletedAssignmentRepository completedAssignmentRepository;
 
-    public CompletedAssignments addCompletedAssignment(CompletedAssignments completedAssignments){
+    public CompletedAssignments addCompletedAssignment(CompletedAssignments completedAssignments) {
         return completedAssignmentRepository.save(completedAssignments);
     }
 
-    public List<CompletedAssignments> getAllbycourseId(Long courseId){
+    public List<CompletedAssignments> getAllbycourseId(Long courseId) {
         return completedAssignmentRepository.findallBycourseId(courseId);
     }
 
-    public List<CompletedAssignments> getAllbyuserId(Long userId){
+    public List<CompletedAssignments> getAllbyuserId(Long userId) {
         return completedAssignmentRepository.findallByuserId(userId);
     }
 
-    public List<CompletedAssignments> getAllbyinstructorId(Long instructorId){
+    public List<CompletedAssignments> getAllbyinstructorId(Long instructorId) {
         return completedAssignmentRepository.findallByInstructorId(instructorId);
     }
 
-    public List<CompletedAssignments> getAllbyassignmentId(Long assignmentId){
+    public List<CompletedAssignments> getAllbyassignmentId(Long assignmentId) {
         return completedAssignmentRepository.findallByAssignmentId(assignmentId);
     }
 }
