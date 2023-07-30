d13028e16881393f6d1885af2e96ddf0cba7aba6
Sumanth42 <nsumanth2112@gmail.com>
2023-07-30 12:08:22 +0000

    updated cross orgins

--- springapp/src/main/java/com/example/springapp/controller/EnrollmentController.java
+++ springapp/src/main/java/com/example/springapp/controller/EnrollmentController.java
@@ -22,7 +22,7 @@ import com.example.springapp.service.EnrollmentService;
 
 @RestController
 //santhosh
-@CrossOrigin(origins = "https://8081-cbbdbceccaaadcdddffaedcbcabfdfafdade.project.examly.io")
+// @CrossOrigin(origins = "https://8081-cbbdbceccaaadcdddffaedcbcabfdfafdade.project.examly.io")
 
 //ramesh
 //@CrossOrigin(origins = "https://8081-febcaabfefeacfadcdddffaedcbcabfdfafdade.project.examly.io")
@@ -37,7 +37,7 @@ import com.example.springapp.service.EnrollmentService;
 //@CrossOrigin(origins = "https://8081-fdfedfdaaaacfedbadcdddffaedcbcabfdfafdade.project.examly.io")
 
 //sumanth
-//@CrossOrigin(origins = "https://8081-daeacaadbcfaeadcdddffaedcbcabfdfafdade.project.examly.io")
+@CrossOrigin(origins = "https://8081-daeacaadbcfaeadcdddffaedcbcabfdfafdade.project.examly.io")
 
 //mrinal
 //@CrossOrigin(origins = "https://8081-fbbfbccabebadcdddffaedcbcabfdfafdade.project.examly.io")
